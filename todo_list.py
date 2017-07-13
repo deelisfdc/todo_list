cfee1ebb9ff29243e9109eeaf2c46481d0093302
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-12 21:17:57 -0700

    finalized program

--- todo_list.py
+++ todo_list.py
@@ -12,13 +12,18 @@ You can run the script in your Terminal at any time using the command:
 def add_to_list(my_list):
     """Takes user input and adds it as a new item to the end of the list."""
 
-    print "The add_to_list function has not yet been written"
+    item = raw_input("What would you like to add? ")
+    print item
+
+            
+    my_list.append(item)
 
 
 def view_list(my_list):
     """Print each item in the list."""
-
-    print "The view_list function has not yet been written"
+    for items in my_list:
+        print items
+    # print "The view_list function has not yet been written"
 
 
 def display_main_menu(my_list):
@@ -30,11 +35,23 @@ def display_main_menu(my_list):
     B. View list
     C. Quit the program
     >>> """
-
+    # print user_options
     while True:
-        # Collect input and include your if/elif/else statements here.
-        break
+        user_choices = raw_input(user_options).upper()
+        if user_choices == "A":
+            add_to_list(my_list)
+        elif user_choices == "B":
+            view_list(my_list)
+        elif user_choices == "C":
+            break  
 
+        #     user_options.upper()
+        #     print "Add new item"
+        #     item = raw_input("What would you like to add? ")
+        #     item.append(my_list)
+         
+        # Collect input and include your if/elif/else statements here.
+       
 #-------------------------------------------------
 
 my_list = []
